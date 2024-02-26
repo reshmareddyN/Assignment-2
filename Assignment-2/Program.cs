9dc2312fe8542ee764f35e441719837c4fe7b65c
Reshma_Nalamalapu <rn21aae@herts.ac.uk>
2024-02-25 20:51:33 -0500

    initialise board

--- Assignment-2/Program.cs
+++ Assignment-2/Program.cs
@@ -7,4 +7,7 @@ public class GemHunters
         char[,] board = new char[6, 6];
         int player1Score = 0;
         int player2Score = 0;
+        // Initialize the board
+        InitializeBoard(board);
+
 
