db164987f39bc7f409790d391d996349e5e6a9c1
Reshma_Nalamalapu <rn21aae@herts.ac.uk>
2024-02-25 20:50:35 -0500

    created header files

--- Assignment-2/Program.cs
+++ Assignment-2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class GemHunters
+{
+    static void Main(string[] args)
+    {
+        char[,] board = new char[6, 6];
+        int player1Score = 0;
+        int player2Score = 0;
+
